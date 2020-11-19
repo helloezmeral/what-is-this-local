1f3f9d53f82e35f680e67964fff4556fd73cc900
helloezmeral <72959956+helloezmeral@users.noreply.github.com>
2020-11-19 16:56:31 +0800

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,11 @@
+# !apt update -qq
+# !apt upgrade -yq
+# !pip install --upgrade pip
+# !pip install jupyterlab
+# !pip install pandas
+# !pip install Pillow
+# !pip install requests
+!DEBIAN_FRONTEND=noninteractive apt install python-opencv
+!pip install opencv-python
+# !apt install wget -qq
+# !pip install tensorflow
